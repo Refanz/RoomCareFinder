a252c093eed9725a7f202b08109014b42151bc0e
Refanzzzz <refandasuryasaputra@gmail.com>
2023-11-19 04:18:44 +0700

    add pagination

--- src/components/dashboard/ListHospitals.jsx
+++ src/components/dashboard/ListHospitals.jsx
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import {useContext, useState} from "react";
 import {HospitalContext} from "../../context/HospitalContext.jsx";
 import ItemHospital from "./ItemHospital.jsx";
-import {Grid} from "@mui/material";
+import {Grid, Pagination} from "@mui/material";
 import SearchHospital from "./SearchHospital.jsx";
 import PropTypes from "prop-types";
 import {useLocation} from "react-router-dom";
@@ -12,6 +12,12 @@ function ListHospitals({type}) {
     const hospitals = useContext(HospitalContext);
     const [search, setSearch] = useState("");
     const location = useLocation();
+
+    const [page, setPage]= useState(1);
+
+    function handlePagination(evt, val) {
+        setPage(val);
+    }
 
     function filteredHospitals() {
         if (search === "") {
@@ -27,11 +33,11 @@ function ListHospitals({type}) {
                 <SearchHospital setSearch={setSearch} />
                 <Grid container spacing={4} mt={1}>
                     {
-                        filteredHospitals("umum").map((hospital, index) =>  <ItemHospital key={index} hospital={hospital} location={location}/>)
+                        filteredHospitals().slice((page - 1) * 3, page * 3).map((hospital, index) =>  <ItemHospital key={index} hospital={hospital} location={location}/>)
                     }
                 </Grid>
+                <Pagination count={Math.ceil((filteredHospitals().length / 2) - 1 )} variant="outlined" color="primary" sx={{marginTop: '30px'}} onChange={handlePagination} page={page} />
             </Container>
-
             <Footer />
         </>
     );
